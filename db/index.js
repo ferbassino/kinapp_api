b81b70b4276cb6b57bf914584a4167a78e963938
Fernando Bassino <ferbassino@gmail.com>
2023-08-15 08:14:30 -0300

    uri

--- db/index.js
+++ db/index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 console.log("process", process.env.MONGO_URI);
+const URI =
+  "mongodb+srv://ferbassino:6U4Xnn6kaZOBjAhl@cluster0.fu84jsk.mongodb.net/kin-app-auth?retryWrites=true&w=majority";
 module.exports = mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(URI)
   .then(() => {
     console.log("kin-app database connected");
   })
