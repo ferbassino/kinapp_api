3506c03f45e96e7290e4b678f74d228d0d9d962f
Fernando Bassino <ferbassino@gmail.com>
2024-10-12 17:43:15 -0300

    socket sin path

--- socket/socket copy.js
+++ socket/socket copy.js
@@ -1,3 +1,4 @@
+// socket.js
 const { Server } = require("socket.io");
 
 const initializeSocket = (server) => {
@@ -6,16 +7,20 @@ const initializeSocket = (server) => {
       origin: "*", // Permitir todos los dominios en desarrollo
       methods: ["GET", "POST"],
     },
+    // path: "/my-custom-socket-path",
   });
 
   // Usar un namespace para que el socket solo se conecte en una ruta específica
   const customNamespace = io.of("/custom-socket");
 
+  // Manejar conexiones de Socket.IO en el namespace
   customNamespace.on("connection", (socket) => {
     console.log("Cliente conectado al namespace custom-socket:", socket.id);
 
+    // Escuchar eventos personalizados
     socket.on("sensorData", (data) => {
-      console.log("Datos recibidos del cliente:", data);
+      //   console.log("Datos recibidos del cliente:", data);
+      // Emitir los datos recibidos a todos los clientes conectados
       customNamespace.emit("sensorData", data);
     });
 
