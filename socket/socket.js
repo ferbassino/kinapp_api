2ffa8e42258680effd507a2ebff24b284a2e95f9
Fernando Bassino <ferbassino@gmail.com>
2024-10-13 11:07:17 -0300

    adding errores

--- socket/socket.js
+++ socket/socket.js
@@ -15,6 +15,12 @@ const initializeSocket = (server) => {
     },
     allowEIO3: true,
   });
+  io.engine.on("connection_error", (err) => {
+    console.log(err.req); // the request object
+    console.log(err.code); // the error code, for example 1
+    console.log(err.message); // the error message, for example "Session ID unknown"
+    console.log(err.context); // some additional error context
+  });
 
   // Usar un namespace para que el socket solo se conecte en una ruta específica
   const customNamespace = io.of("/custom-socket");
